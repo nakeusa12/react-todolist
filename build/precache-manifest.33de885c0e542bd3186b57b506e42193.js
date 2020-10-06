2574d0339dcf35a69be8fcdf02f8d2d11eff9892
Saiful Nakeusa <snakeusa8@gmail.com>
2020-10-06 21:18:54 +0700

    build

---
+++ build/precache-manifest.33de885c0e542bd3186b57b506e42193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc43d547cfc4074b23c9cff3e3e40ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3387c7f152925340480b",
+    "url": "/static/css/main.7dc54a31.chunk.css"
+  },
+  {
+    "revision": "318e890f4c9c4492a031",
+    "url": "/static/js/2.75ece39e.chunk.js"
+  },
+  {
+    "revision": "3387c7f152925340480b",
+    "url": "/static/js/main.3fc2a952.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
